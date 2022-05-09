c8eb2ce3fe209d165e97d8d95a673c24b0ff7a95
Alina Andreevna <lkj452kl@gmail.com>
2022-05-09 12:06:27 +0300

    hello

---
+++ app/src/main/java/com/example/paffagorapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.paffagorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.paffagorapp.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClickResult(view: View) {
+if (!isFieldEmpty()){
+    val result = getString(R.string.result_info) + getResult()
+    binding.textResult.text = result
+}
+    }
+
+    private fun isFieldEmpty(): Boolean{
+binding.apply {
+    if (edA.text.isNullOrEmpty()) edA.error = "Поле должно быть заполненно"
+    if (edB.text.isNullOrEmpty()) edB.error = "Поле должно быть заполненно"
+    return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+}
+    }
+private fun getResult(): String{
+    val a: Double
+    val b: Double
+    binding.apply {
+        a = edA.text.toString().toDouble()
+        b = edB.text.toString().toDouble()
+    }
+    return sqrt( (a.pow(2)+b.pow(2)) ).toString()
+}
+}
